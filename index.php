f204d3b8b105602f6b5a5ba6759214c0a721deed
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-20 16:20:54 -0300

    Inicio banner

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ Template Name: Inicio
 <!-- BANNER PRINCIPAL -->
 <div class="container-fluid banner texto-centrado">
     <div class="row texto-centrado">
-        <div class="col info-banner">
-            <h1>Dejate llevar por la potencia de la música</h1>
+        <div class="col info-banner mr-5 ml-5">
+            <h1>Dejate llevar por la potencia <br> de la música</h1>
             <h3>Disfruta de la música compartida en tiempo real.</h3>
             <button type="button" class="btn btn-light">Iniciar sesión</button>
         </div>
