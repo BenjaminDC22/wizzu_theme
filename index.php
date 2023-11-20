359b2fecb30c449df0665b27660bf945815f47e1
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-20 16:45:15 -0300

    Calidad

--- index.php
+++ index.php
@@ -17,4 +17,32 @@ Template Name: Inicio
     </div>
 </div>
 
+<!-- CALIDAD DE SONIDO -->
+<div class="container">
+    <h2>Un sonido enérgico para cada ocasión</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="calidad">
+                <h4>Max</h4>
+                <p>(Hasta 24 bit, 192 kHz)</p>
+            </div>
+            <p>Disfruta de un audio inigualable que reproduce cada detalle con HiRes Free Lossless Audio Code (HiRes FLAC). La experiencia será mucho mejor con una conexión 5G o Wifi con conexión de hardware.</p>
+        </div>
+        <div class="col-md-4">
+            <div class="calidad">
+                <h4>ALTA</h4>
+                <p>(Hasta 16 bit, 44,1 kHz)</p>
+            </div>
+            <p>Disfruta de más de 100 millones de canciones en calidad de estudio con FLAC. Gracias a su formato de código abierto, cada artista puede crear y ofrecer música de alta fidelidad de forma sencilla.</p>
+        </div>
+        <div class="col-md-4">
+            <div class="calidad">
+                <h4>BAJA</h4>
+                <p>(Hasta 320 kbps)</p>
+            </div>
+            <p>Disfruta de la música que te gusta mientras viajas sin preocuparte por los datos. Algo extremadamente útil cuando tienes mala cobertura de datos, se te están acabando o te estás quedando sin espacio para más descargas.</p>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
