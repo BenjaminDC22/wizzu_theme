bfb518a2f81d8f71f62054425e55cee82950c708
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-20 16:19:22 -0300

    Fuentes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Template Name: Inicio
 <!-- BANNER PRINCIPAL -->
 <div class="container-fluid banner texto-centrado">
     <div class="row texto-centrado">
-        <div class="col">
+        <div class="col info-banner">
             <h1>Dejate llevar por la potencia de la música</h1>
             <h3>Disfruta de la música compartida en tiempo real.</h3>
             <button type="button" class="btn btn-light">Iniciar sesión</button>
