5982c0bdbdeb0741c4892a7402341a32e296fd6d
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-20 21:32:22 -0300

    iconos

--- feed.php
+++ feed.php
@@ -15,6 +15,18 @@ Template Name: Feed
                     <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/search.png">
                     <h5 class="mb-0">Buscar</h5>
                 </button>
+                <button class="boton-wizzu w-100">
+                    <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/home.png">
+                    <h5 class="mb-0">Inicio</h5>
+                </button>
+                <button class="boton-wizzu w-100">
+                    <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/reloj.png">
+                    <h5 class="mb-0">Recientes</h5>
+                </button>
+                <button class="boton-wizzu w-100">
+                    <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/radio.png">
+                    <h5 class="mb-0">Radio</h5>
+                </button>
             </div>
         </div>
 
