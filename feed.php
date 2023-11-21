3cbcefbda14373ac55b19c759fbbfca8913bf491
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-20 23:37:09 -0300

    f

--- feed.php
+++ feed.php
@@ -54,9 +54,9 @@ Template Name: Feed
         </div>
 
         <!-- Lado derecho -->
-        <div class="col-md-10">
+        <div class="col-md-9">
             <div class="pantalla-wizzu">
-                <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/banner-feed">
+                <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/banner-feed.jgp">
             </div>
         </div>
     </div>
