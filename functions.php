3733464af36fed211c0b8ed91cb32926183acf70
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-21 03:09:26 -0300

    h

--- functions.php
+++ functions.php
@@ -107,14 +107,4 @@ register_nav_menu('main-menu', 'Main menu');
 
 
 
-
-/* Llamado al JS */
-
-add_action('wp_enqueue_scripts', 'mi_js_personalizado');
-
-function mi_js_personalizado(){
-  wp_register_script('index', get_stylesheet_directory_uri().'/js/script.js', array('jquery'), '1.0.0', true);
-  wp_enqueue_script('index');
-}
-
 ?>
