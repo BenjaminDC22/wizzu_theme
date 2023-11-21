814a59e6f06777636aa6826420ddd7ecba6c79d0
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-20 21:19:52 -0300

    a

--- feed.php
+++ feed.php
@@ -6,6 +6,26 @@ Template Name: Feed
 
 <?php get_header(); ?>
 
+<!-- INTERFAZ COMPLETA -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="menu-wizzu">
+                <button>
+                    <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/search.png">
+                    Hola
+                </button>
+            </div>
+        </div>
 
+
+
+        <div class="col-md-8">
+            <div class="pantalla-wizzu">
+
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
