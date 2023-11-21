873a177a23696ede8328d4b6ed099b8b40c4f73a
BenjaminDC22 <ben.abarcas@duocuc.cl>
2023-11-20 23:25:34 -0300

    j

--- feed.php
+++ feed.php
@@ -10,6 +10,7 @@ Template Name: Feed
 <div class="container seccion-menu">
     <div class="row">
         <div class="col-md-3">
+            <!-- Menú principal -->
             <div class="menu-wizzu mb-3">
                 <button class="boton-wizzu w-100">
                     <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/search.png">
@@ -25,6 +26,7 @@ Template Name: Feed
                 </button>
             </div>
 
+            <!-- Menú de biblioteca -->
             <div class="menu-wizzu">
                 <h3 class="mb-3">Tu biblioteca</h3>
                 <button class="boton-wizzu w-100">
@@ -39,9 +41,13 @@ Template Name: Feed
                     <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/melodia.png">
                     <h5 class="mb-0">Canciones</h5>
                 </button>
-                <button class="boton-wizzu w-100 mt-3">
+                <button class="boton-wizzu w-100 my-3">
                     <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/microfono.png">
                     <h5 class="mb-0">Artistas</h5>
+                </button>
+                <button class="boton-wizzu w-100 mt-3">
+                    <img class="iconos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/usuario.png">
+                    <h5 class="mb-0">Para ti</h5>
                 </button>
             </div>
         </div>
